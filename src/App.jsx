411c768342cce17545988b7bbb96b2921007d005
akiwnl <carvalhokaike69@gmail.com>
2024-10-25 18:34:10 -0300

    update RepositoryList component

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 export function App() {
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
 }
