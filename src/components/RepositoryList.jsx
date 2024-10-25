f30b1525c6bd78c7e9dab277c18c1cc9d205687c
akiwnl <carvalhokaike69@gmail.com>
2024-10-25 18:35:06 -0300

    add useState & useEffect to fetch github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,23 @@
+import { useState, useEffect } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/akiwnl/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositórios</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositórios</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositórios</a>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
