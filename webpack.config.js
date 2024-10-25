884ab1dca734b15cd439132c1f2d05f40759eec6
akiwnl <carvalhokaike69@gmail.com>
2024-10-24 21:47:47 -0300

    add development and production mode & sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"), // Caminho absoluto para a pasta de saída
@@ -31,6 +34,11 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"], // Aqui removi o objeto e usei diretamente um array
       },
     ],
   },
